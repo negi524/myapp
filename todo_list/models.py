567e0430106091c4e2cc7f6c6dbcf8a1e7156b1d
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-09-27 01:29:09 +0900

    add str methods

--- todo_list/models.py
+++ todo_list/models.py
@@ -7,11 +7,18 @@ class Content (models.Model):
     content_name = models.CharField(max_length=200)
     content_desc = models.CharField(max_length=400)
 
+    def __str__(self):
+        return self.content_name
+
 
 class Ticket (models.Model):
     """やることリストのチケットのモデル
     """
     ticket_id = models.CharField(max_length=20)
+    ticket_name = models.CharField(max_length=50)
     created_date = models.DateField('date created')
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
     weight = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.ticket_name
