56773e11a979cda99a502c8a38bb207794375c29
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-09-27 01:14:42 +0900

    create models

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Content (models.Model):
+    """やる内容のモデル
+    """
+    content_name = models.CharField(max_length=200)
+    content_desc = models.CharField(max_length=400)
+
+
+class Ticket (models.Model):
+    """やることリストのチケットのモデル
+    """
+    ticket_id = models.CharField(max_length=20)
+    created_date = models.DateField('date created')
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    weight = models.IntegerField(default=0)
