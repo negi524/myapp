e46c7ef0e14fd46ffe6b2a89ca01e1f01395842e
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-09-28 15:53:00 +0900

    create admin user

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ticket
+from .models import Content
+
+admin.site.register(Ticket)
+admin.site.register(Content)
--- todo_list/views.py
+++ todo_list/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, World. You're at the todo_list index.")
+
+
+def detail(request, ticket_id):
+    return HttpResponse("You're looking at the detail of %s." % ticket_id)
