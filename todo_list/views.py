61863a5ebd3967fd18b5f3cd524f054245847c9c
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-09-28 16:25:38 +0900

    render template

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Ticket, Content
 
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the todo_list index.")
+    all_ticket = Ticket.objects.all()
+    template = loader.get_template('todo_list/index.html')
+    context = {
+        'ticket_name': all_ticket,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, ticket_id):
