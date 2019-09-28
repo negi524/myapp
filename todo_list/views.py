78f0efa0168552011c64f290357c48de428b7374
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-09-28 16:27:47 +0900

    simplification

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Ticket, Content
 
 
 def index(request):
     all_ticket = Ticket.objects.all()
-    template = loader.get_template('todo_list/index.html')
     context = {
         'ticket_name': all_ticket,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'todo_list/index.html', context)
 
 
 def detail(request, ticket_id):
